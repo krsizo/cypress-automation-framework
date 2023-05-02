c4995a4039f436ed237c62874c6a4235d15a08a9
krsizo <kristina77sizova@gmail.com>
2023-05-02 21:55:40 +0300

    Fixes

--- cypress/e2e/automation-test-store/contact-us.js
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -7,7 +7,7 @@ describe("Test Contact Us form via Automation Test Store", () => {
     })
     it("Should be able to submit a successful submission via contact us form", () => {
         cy.visit("https://www.automationteststore.com/");
-        cy.get("a[href$='contac0t']").click().then(function(linkText) {
+        cy.get("a[href$='contact']").click().then(function(linkText) {
             cy.log("Selected the following link: " + linkText.text());
         });
         //cy.xpath("//a[contains(@href, 'contact')]").click();
